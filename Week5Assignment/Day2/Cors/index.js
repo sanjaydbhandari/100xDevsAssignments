8a724bf4b00c7f08af61e743b56eb598f1261bd6
sanjaydbhandari <sanjaybhandari1211@gmail.com>
2024-09-12 20:57:34 +0530

    add cors assignment

--- Week5Assignment/Day2/Cors/index.js
+++ Week5Assignment/Day2/Cors/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express = require("express");
 const app = express();
 
@@ -10,3 +11,24 @@ app.post("/add", (req, res) => {
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`app is running on port ${PORT}`));
+=======
+const express = require('express');
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/add",function(req,res){
+    console.log(parseInt(req.body.a)+parseInt(req.body.b));
+    res.json({
+        "result":parseInt(req.body.a)+parseInt(req.body.b)
+    })
+});
+
+const PORT=3333
+app.listen(PORT,()=>{
+    console.log(`app running on port ${PORT}`);
+})
+>>>>>>> 406344b (add cors assignment)
